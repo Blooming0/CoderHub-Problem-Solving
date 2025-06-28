4647fc01a1e13e212059dc71d06d471cada92eb1
Blooming0 <hassanalhosyni@gmail.com>
2025-06-28 14:34:34 +0300

    Stack structure
    Solving the problem of postfix Operation
    
    Co-Authored-By: Hassan-Alhussaini <154713499+blooming0@users.noreply.github.com>

--- src/PostfixOperationHARD.java
+++ src/PostfixOperationHARD.java
@@ -1,61 +1,35 @@
 import java.util.regex.*;
+import java.util.Stack;
 public class PostfixOperationHARD {
     public static void main(String[] args) {
         System.out.println(postFix("10 1 + 11 /"));
 
     }
     public static int postFix(String expr) {
+        Stack<Integer> stack = new Stack<>();
+        String[] tokens = expr.trim().split("\\s+");
 
-        String nums = "0123456789";
-        String []numsAndOperationsWithoutSpaces = new String[expr.length() - 1];
-        int counter = 0 ;
-        //حفظ الارقام مع العمليات بدون التعديل على الترتيب ولكن بازالة المسافات
-        for (int i = 0; i < expr.length(); i++) {
-            if(expr.charAt(i) == ' '){
-                continue;
+        for (String token : tokens) {
+            if (isNumeric(token)) {
+                stack.push(Integer.parseInt(token));
+            } else {
+                int b = stack.pop();
+                int a = stack.pop();
 
-            }else {
-                    if(expr.charAt(i) == '+' || expr.charAt(i) == '-' || expr.charAt(i) == '*' || expr.charAt(i) == '/' ){
-                        numsAndOperationsWithoutSpaces[counter++] = String.valueOf(expr.charAt(i));
-                    }else {
-                        if(expr.charAt(i) >= '0' &&  expr.charAt(i) <= '9' && expr.charAt(i+1) == ' '){
-                            numsAndOperationsWithoutSpaces[counter++] = String.valueOf(expr.charAt(i));
-                        }else if(expr.charAt(i) >= '0' &&  expr.charAt(i) <= '9' && expr.charAt(i+1) != ' '){
-                            String numberFromTwoDigit = expr.charAt(i)+""+expr.charAt(i+1);
-                            numsAndOperationsWithoutSpaces[counter++] = numberFromTwoDigit;
-                            i++;
-                    }
+                switch (token) {
+                    case "+": stack.push(a + b); break;
+                    case "-": stack.push(a - b); break;
+                    case "*": stack.push(a * b); break;
+                    case "/": stack.push(a / b); break;
                 }
+            }
+        }
 
-            }
-        }
-        for (int i = 0; i < numsAndOperationsWithoutSpaces.length - 1; i++) {
-            if(("+").equals(numsAndOperationsWithoutSpaces[i+1]) || ("-").equals(numsAndOperationsWithoutSpaces[i+1]) ||("*").equals(numsAndOperationsWithoutSpaces[i+1]) ||("/").equals(numsAndOperationsWithoutSpaces[i+1])){
-                String temp = numsAndOperationsWithoutSpaces[i];
-                numsAndOperationsWithoutSpaces[i] = numsAndOperationsWithoutSpaces[i+1];
-                numsAndOperationsWithoutSpaces[i+1] = temp;
-            }
-        }
-        int result = 0;
-        for (int i = 0; i < numsAndOperationsWithoutSpaces.length - 1; i++) {
+        return stack.pop();
+    }
 
-            if(numsAndOperationsWithoutSpaces[i].equals("+")){
-                result += Integer.parseInt(numsAndOperationsWithoutSpaces[i + 1]) + Integer.parseInt(numsAndOperationsWithoutSpaces[i - 1]);
-                i = i+2;
-            }else if(numsAndOperationsWithoutSpaces[i].equals("-")){
-                result += Integer.parseInt(numsAndOperationsWithoutSpaces[i + 1]) - Integer.parseInt(numsAndOperationsWithoutSpaces[i - 1]);
-                i = i+2;
-            }else if(numsAndOperationsWithoutSpaces[i].equals("*")){
-                result += Integer.parseInt(numsAndOperationsWithoutSpaces[i + 1]) * Integer.parseInt(numsAndOperationsWithoutSpaces[i - 1]);
-                i = i+2;
-            }else if(numsAndOperationsWithoutSpaces[i].equals("/")){
-                result += Integer.parseInt(numsAndOperationsWithoutSpaces[i + 1]) / Integer.parseInt(numsAndOperationsWithoutSpaces[i - 1]);
-                i = i+2;
-            }else if(numsAndOperationsWithoutSpaces[i] == null){
-                break;
-            }
-        }
-        return result ;
+    private static boolean isNumeric(String str) {
+        return str.matches("-?\\d+");
     }
 
 
